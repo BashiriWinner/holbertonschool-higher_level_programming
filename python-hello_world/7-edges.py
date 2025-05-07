83ead3694e4ca304e8688c845a8fb1a16322447e
BashiriWinner <abeshiri.ab@gmail.com>
2025-05-07 21:39:47 +0400

    7-edges.py

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = word[0:2]
 word_last_2 = word[-2:]
-middle_word = word[0:5]
+middle_word = word[1:5]
 print(f"first 3 letters {word_first_3}")
 print(f"last 2 letters {word_last_2}")
 print(f"middle word {middle_word}")
