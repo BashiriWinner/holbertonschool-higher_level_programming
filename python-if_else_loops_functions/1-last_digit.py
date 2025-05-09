5b0e37c7413594abad5dbc91ad417948490fa2de
BashiriWinner <abeshiri.ab@gmail.com>
2025-05-09 21:23:12 +0400

    rundit fix

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 0)
+number = random.randint(-10000, 10000)
 last = number % 10
 last_m = last - 10
 if number > 0:
@@ -13,5 +13,3 @@ if number > 0:
 else:
     if last != 0:
         print(f"Last digit of {number} is {last_m} and is less than 6 and not 0")   
-    else:
-        print(f"Last digit of {number} is 0 and is 0")
