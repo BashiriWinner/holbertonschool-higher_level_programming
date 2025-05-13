11642d217097ff4d97ddafc861f04e9f5e1dd656
BashiriWinner <abeshiri.ab@gmail.com>
2025-05-13 18:48:34 +0400

    1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import
+if_name_ == "_main_":
+    a = 10
+    b = 5
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+
