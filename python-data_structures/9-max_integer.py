e785f0835ac05f3079077181c2776c216d8f7f80
BashiriWinner <abeshiri.ab@gmail.com>
2025-05-15 18:38:50 +0400

    9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max_num = my_list[]
+    for num in my list:
+        if num > max_num:
+            max_num = num
+            return max_num
+
