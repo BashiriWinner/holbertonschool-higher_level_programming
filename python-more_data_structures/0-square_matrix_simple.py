cecc91ce16afad4cb562dcd18e103c01a9eefae8
BashiriWinner <abeshiri.ab@gmail.com>
2025-05-16 18:11:18 +0400

    fix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(element ** 2)
-            new_matrix.append(new_row)
-        return new_matrix
+    new_array = []
+    for arr in matrix:
+        new_arr.append(list(map(lambda x: pow(x, 2), arr)))
+        return new_arr
