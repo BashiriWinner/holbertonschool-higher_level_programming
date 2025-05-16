606da64f20016dafbda9981ea66dfbc09c0ac342
BashiriWinner <abeshiri.ab@gmail.com>
2025-05-16 18:13:09 +0400

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_array = []
+    new_arr = []
     for arr in matrix:
         new_arr.append(list(map(lambda x: pow(x, 2), arr)))
-        return new_arr
+    return new_arr
+
