91d674e23bad02f6e56a034c413060d85f2de727
BashiriWinner <abeshiri.ab@gmail.com>
2025-05-17 12:23:48 +0400

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    if length == 0:
-        first_char = None
-    else:
-        first_char = sentence[0]
-        return (length, first_char)
+    if len(sentence):
+        return len(sentence), sentence[0]
+    return 0, None
+
