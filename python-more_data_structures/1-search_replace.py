4b1ede44edae5b730c77e1daa365ce500c87d132
BashiriWinner <abeshiri.ab@gmail.com>
2025-05-17 12:52:15 +0400

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = my_list.copy()
-    for i in range(0, len(my_list)):
-        if my_list[i] == search:
-            new.remove(search)
-            new.insert(i, replace)
-            return new
+    new_list = [replace if item == search else item for item in my_list]
+    return new_list
+
