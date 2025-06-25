83caf8b646ad1f3e16d4e223a9481a0e0216ca36
root <root@ip-10-42-234-205.ec2.internal>
2025-06-25 13:25:29 +0000

    9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- creates a table second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT exists second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO secon_table(id, name, score) VALUES
+(1, "JOHN", 10);
+(2, "ALEX", 3);
+(3, "BOB", 14);
+(4, "GEORGE",8);
