6a5007ea34ad4d72c9760c237bc38eecc8050ecc
amil <abeshiri.ab@gmail>
2025-06-26 18:10:05 +0400

    4-never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- create the table id_not_null
-CREATE TABLE IF NOT exists id_not_null(id INT, name VARCHAR(256) VALUE(1);
+CREATE TABLE IF NOT exists id_not_null(id INT DEFAULT 1, name VARCHAR(256));
