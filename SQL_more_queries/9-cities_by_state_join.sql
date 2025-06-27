9f0ed935aa995590809176c2d904fe44e374dda6
amil <abeshiri.ab@gmail>
2025-06-27 14:10:58 +0400

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- list all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name FROM cities
 INNER JOIN states ON cities.id = states.id
-ORDER BY id ASC;
+ORDER BY cities.id ASC;
