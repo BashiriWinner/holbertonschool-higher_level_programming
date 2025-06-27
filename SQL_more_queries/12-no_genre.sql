8dd904496d4a938e818ec1d7eb45329d548bbc19
amil <abeshiri.ab@gmail>
2025-06-27 17:11:10 +0400

    12-no_genre.sql

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tw_shows
 LEFT JOIN tv_show_genres ON tw_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title ASC,  tv_show_genres.genre_id ASC;
