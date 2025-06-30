58317543ca86dcc8027e96ebc789df4588377d50
amil <abeshiri.ab@gmail>
2025-06-30 14:45:22 +0400

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     db = mySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=databse)
 
     cursor = db.cursor()
-    cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC)
+    cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     rows = cursor.fetchall()
 
