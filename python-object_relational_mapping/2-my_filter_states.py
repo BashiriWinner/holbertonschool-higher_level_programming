9dadc83fb7c05dbe8b48b409d60b1edc3b5759e1
amil <abeshiri.ab@gmail>
2025-07-01 14:57:25 +0400

    2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script connects to the hbtn_0e_0_usa database and lists all states
+with a name matching the user-provided argument, sorted by id in ascending order."""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db == MySQLdb.connect(user=argv[1], passwd=argv[2], database=argv[3], statename=argv[4])
+
+    cursor = db.cursor()
+
+    cursor.execute("Select id, name FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name))
+
+    for state in cursor.fetchall():
+        print(state)
+
+    cursor.close()
+    db.close()
+
