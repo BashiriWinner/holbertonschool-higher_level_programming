9beaf74cb90ddd7afe5cc86b6b4f6e5d7c02c05d
amil <abeshiri.ab@gmail>
2025-07-01 16:12:19 +0400

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,4 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """This script safely connects to the hbtn_0e_0_usa database and lists all states
 with a name matching the user-provided argument, sorted by id in ascending order."""
 
