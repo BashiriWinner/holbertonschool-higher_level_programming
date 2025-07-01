941f89312c781d3849a1b922ab0e3f7dae70e924
amil <abeshiri.ab@gmail>
2025-07-01 18:56:26 +0400

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""This script safely connects to the hbtn_0e_4_usa database 
+and lists all cities of a user-specified state, 
+sorted by city id in ascending order."""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT cities.name FROM \
+            cities JOIN states ON cities.state_id = states.id \
+            WHERE states.name = %s ORDER BY cities.id ASC")
+
+    cities = []
+    for city in cursor.fetchall():
+        if city[1] == argv[4]:
+            cities.append(city[0])
+
+
+    print(", ".join(cities))
+
+    cursor.close()
+    db.close()
+
