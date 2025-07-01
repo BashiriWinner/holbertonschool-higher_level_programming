4f0cbe8074219ba3a028d8365d59d2501adf1876
amil <abeshiri.ab@gmail>
2025-07-01 18:06:55 +0400

    pycode

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This script connects to the hbtn_0e_4_usa database and lists all cities
-along with their corresponding state names, sorted by city id in ascending order."""
+along with their corresponding state names, 
+sorted by city id in ascending order."""
 
 if __name__ == "__main__":
     from sys import argv
@@ -10,7 +11,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+                   FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
 
     for state in cursor.fetchall():
         print(state)
