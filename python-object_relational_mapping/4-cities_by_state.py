ea2ecec2e6d854949943d4308e73379845c93d5a
amil <abeshiri.ab@gmail>
2025-07-01 17:52:40 +0400

    4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script connects to the hbtn_0e_4_usa database and lists all cities
+along with their corresponding state names, sorted by city id in ascending order."""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+
+    for state in cursor.fetchall():
+        print(state)
+
+    cursor.close()
+    db.close()
+
