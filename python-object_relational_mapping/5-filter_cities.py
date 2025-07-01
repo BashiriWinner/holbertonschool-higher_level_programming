5a0b0a456ef0638dc90112dfc6776b4eaa26689e
amil <abeshiri.ab@gmail>
2025-07-01 19:06:06 +0400

    pycode

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""This script safely connects to the hbtn_0e_4_usa database 
-and lists all cities of a user-specified state, 
+"""This script safely connects to the hbtn_0e_4_usa database
+and lists all cities of a user-specified state,
 sorted by city id in ascending order."""
 
 if __name__ == "__main__":
@@ -19,10 +19,7 @@ if __name__ == "__main__":
     for city in cursor.fetchall():
         if city[1] == argv[4]:
             cities.append(city[0])
-
-
     print(", ".join(cities))
 
     cursor.close()
     db.close()
-
