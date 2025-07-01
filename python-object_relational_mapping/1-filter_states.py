f4337e6214c3d79f8f33227aac2f80fedbf4facd
amil <abeshiri.ab@gmail>
2025-07-01 18:28:33 +0400

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""Module"""
+"""This script connects to the hbtn_0e_0_usa database and lists all states
+with a name starting with 'N', sorted by id in ascending order."""
+
 if __name__ == '__main__':
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(
-        user=argv[1],
-        password=argv[2],
-        database=argv[3]
-    )
+
+    db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3])
+
     cursor = db.cursor()
-    cursor.execute("SELECT * \
-                    FROM `states` \
-                    ORDER BY id")
+    cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
     for state in cursor.fetchall():
-        if state[1] == argv[4]:
+        if state[1][0] == "N":
             print(state)
-    if cursor:
-        cursor.close()
-    if db:
-        db.close()
+
+    cursor.close()
+    db.close()
+
