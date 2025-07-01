d80d95e8c9cdd711d59d0eadc25d59a8c69c4ca9
amil <abeshiri.ab@gmail>
2025-07-01 18:33:06 +0400

    pycode

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This script connects to the hbtn_0e_0_usa database and lists all states
-with a name matching the user-provided argument, sorted by id in ascending order."""
+with a name matching the user-provided argument, 
+sorted by id in ascending order."""
 
 if __name__ == "__main__":
     from sys import argv
@@ -10,7 +11,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("Select id, name FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+    cursor.execute("Select id, name FROM states \
+                   WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
 
     for state in cursor.fetchall():
         print(state)
