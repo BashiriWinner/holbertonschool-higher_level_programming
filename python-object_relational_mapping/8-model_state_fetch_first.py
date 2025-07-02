d25ffcf02cf1040f6330899c4ecda964ac4c2ac5
amil <abeshiri.ab@gmail>
2025-07-02 19:12:51 +0400

    8-model_state_fetch_first.py

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This script prints the first State object 
+from the hbtn_0e_6_usa database
+using SQLAlchemy, based on the lowest id."""
+
+from sqlalchemy import crate_engine
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    username, password, database), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+
+    if state:
+        print(f"{state.id}: {state.name}")
+    else:
+        print("Nothing")
+
+    session.close()
+
