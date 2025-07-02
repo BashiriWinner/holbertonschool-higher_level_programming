2da33d3102d5197f58ddee1dce79c867f2c4f570
amil <abeshiri.ab@gmail>
2025-07-02 19:36:23 +0400

    9-model_state_filter_a.py

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This script lists all State objects 
+with a name containing the letter 'a'
+from the hbtn_0e_6_usa database 
+using SQLAlchemy, sorted by id."""
+
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name.like('%a%')).order_by(state.id).all()
+
+    for state in states:
+    print(f"{state.id}: {state.name}")
+
+    session.close()
+
