71bbd940a84864627ad52ddd9a0f616e020d5a1e
amil <abeshiri.ab@gmail>
2025-07-02 20:41:17 +0400

    10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This script prints the State object's id with the given name
+from the hbtn_0e_6_usa database using SQLAlchemy."""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
+
