42abebc2aa4f0da28784b9db632d9f71a49cee37
amil <abeshiri.ab@gmail>
2025-07-02 20:49:11 +0400

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     session = sessionmaker(bind=engine)
     session = Session()
 
-    state = (session.query(State).filter(State.name == state_name).first())
+    state = (session.query(State).filter(State.name == argv[4]).first())
 
 
     if state:
