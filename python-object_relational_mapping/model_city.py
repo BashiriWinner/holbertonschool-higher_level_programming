e2f08c8f0dc85e6664e7371a03d30fc98d8e6a5d
amil <abeshiri.ab@gmail>
2025-07-03 18:45:51 +0400

    model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -5,6 +5,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+Base = declarative_base()
+
 class City(Base):
     __tablename__ == "cities"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
