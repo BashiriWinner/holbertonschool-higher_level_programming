c20ed352f3a9b6fdd9d268dc90c32b4a5c1f941a
amil <abeshiri.ab@gmail>
2025-07-03 19:18:36 +0400

    14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,6 +6,7 @@ from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import City
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
