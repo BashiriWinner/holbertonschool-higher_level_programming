98e4e55bbb836e2632cdbb9b817d27b7ba759b27
amil <abeshiri.ab@gmail>
2025-07-17 19:24:27 +0400

    Fix string quotes for consistency in 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,8 @@ const arg = process.argv[2];
 const num = parseInt(arg);
 
 if (arg === undefined || isNaN(num)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
-  console.log("My number: " + num);
+  console.log('My number: ' + num);
 }
+
