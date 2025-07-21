ece41b28f94a99ba44c55797e7b395bd80616d56
amil <abeshiri.ab@gmail>
2025-07-21 15:07:29 +0400

    fix: standardize string quotes in 7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@ const x = process.argv[2];
 const num = parseInt(x);
 
 if (isNaN(num) || x === undefined) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else if (num > 0) {
   for (let i = 0; i < num; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 }
