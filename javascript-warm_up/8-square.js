9fb8690650e18d69ff356a01d333592811ef2eb5
amil <abeshiri.ab@gmail>
2025-07-21 15:13:08 +0400

    feat: add square printing functionality in 8-square.js

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const size = process.argv[2];
+const num = parseInt(size);
+
+if (isNaN(num) || size === undefined || num <= 0) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log("X".repeat(num));
+  }
+}
