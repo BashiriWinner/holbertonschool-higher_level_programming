98393c1a00c7155a36ea698fa54d609d0501a93c
amil <abeshiri.ab@gmail>
2025-07-21 16:11:05 +0400

    fix: correct formatting and add newline at end of file in 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,6 @@ const numbers = args.map(num => parseInt(num));
 if (args.length <= 1) {
   console.log(0);
 } else {
-   const sorted = numbers.sort((a, b) => b - a);
-   console.log(sorted[1]);
-   }
-   
+  const sorted = numbers.sort((a, b) => b - a);
+  console.log(sorted[1]);
+ }
