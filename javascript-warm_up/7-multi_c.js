7dd0711c4cf236ebbc403fa62f2e13f3330eb7cf
amil <abeshiri.ab@gmail>
2025-07-21 15:04:32 +0400

    fix: improve input validation in 7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+#!/usr/bin/env node
 const x = process.argv[2];
 const num = parseInt(x);
 
-if (isNaN(num) || x === undefined || num <= 0) {
+if (isNaN(num) || x === undefined) {
   console.log("Missing number of occurrences");
-} else {
+} else if (num > 0) {
   for (let i = 0; i < num; i++) {
     console.log("C is fun");
   }
