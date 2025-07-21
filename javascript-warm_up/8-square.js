79f25c18233167f846b8dbc3898cb44760b62674
amil <abeshiri.ab@gmail>
2025-07-21 15:14:55 +0400

    fix: standardize string quotes in 8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const size = process.argv[2];
 const num = parseInt(size);
 
 if (isNaN(num) || size === undefined || num <= 0) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
-    console.log("X".repeat(num));
+    console.log('X'.repeat(num));
   }
 }
