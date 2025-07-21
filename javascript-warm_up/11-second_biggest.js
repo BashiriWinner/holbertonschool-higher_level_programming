d2914fe0d833f7cc91b42c01dec800b1f0da73f3
amil <abeshiri.ab@gmail>
2025-07-21 16:01:40 +0400

    feat: implement second biggest number finder in 11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const numbers = args.map(num => parseInt(num));
+if (args.length <= 1) {
+  console.log(0);
+} else {
+const sorted = numbers.sort((a, b) => b - a);
+console.log(sorted[1]); 
+}
