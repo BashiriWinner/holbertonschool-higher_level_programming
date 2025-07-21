32060cb78400a4975b953f4c2d0b3aad44f11207
amil <abeshiri.ab@gmail>
2025-07-21 15:37:48 +0400

    feat: add factorial function implementation in 10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const n = process.argv[2];
+function factorial(n) {
+  const num = parseInt(n);
+  if (isNaN(num) || num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
