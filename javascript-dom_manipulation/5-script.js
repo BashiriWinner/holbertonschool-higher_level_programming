16d2fdce455616530eef1d6b36f271e19bdb4fa8
amil <abeshiri.ab@gmail>
2025-07-22 12:09:43 +0400

    refactor: simplify event listener for header update

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,11 +1,4 @@
-const updateHeaderDiv = document.getElementById('update_header');
-const header = document.querySelector('header');
-
-updateHeaderDiv.addEventListener('click', () => {
-  header.textContent = 'New Header!!!';
-});const updateHeaderDiv = document.getElementById('update_header');
-const header = document.querySelector('header');
-
-updateHeaderDiv.addEventListener('click', () => {
-  header.textContent = 'New Header!!!';
+document.getElementById("update_header").addEventListener("click", function()
+{
+    document.querySelector("header").textContent = "New Header!!!";
 });
