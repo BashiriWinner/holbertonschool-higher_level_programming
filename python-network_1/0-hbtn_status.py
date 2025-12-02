58208a6f5b8b7e35a1002fb27f4e77777a6dc3fe
amil <abeshiri.ab@gmail.com>
2025-12-02 15:04:41 +0400

    Fix whitespace issues in 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,6 +4,8 @@ Fetches https://intranet.hbtn.io/status with custom header to bypass firewall.
 """
 
 import urllib.request
+
+
 def fetch_status():
     url = 'https://intranet.hbtn.io/status'
     req = urllib.request.Request(url)
@@ -16,6 +18,7 @@ def fetch_status():
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
     print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
 
 if __name__ == "__main__":
     fetch_status()
