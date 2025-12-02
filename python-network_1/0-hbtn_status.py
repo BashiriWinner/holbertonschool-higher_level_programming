80673ec52001a49f6b9c7ff43da45c9e0ec5f4e2
amil <abeshiri.ab@gmail.com>
2025-12-02 07:04:49 +0000

    0-hbtn_status.py

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,22 @@
+#!/user/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status with custom header to bypass firewall.
+"""
+
+import urllib.request
+
+def fetch_status():
+    url = 'https://intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    req.add_header('cfclearance', 'true')
+
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
+if __name__ == "__main__":
+    fetch_status()
