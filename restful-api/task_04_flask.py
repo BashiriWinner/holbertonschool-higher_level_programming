95b58365baf72ff2d01d968340f7d6d09e5753f4
amil <abeshiri.ab@gmail.com>
2025-12-05 18:03:21 +0400

    task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,50 +1,37 @@
-from flask import Flask, jsonify, request # type: ignore
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
 users = {}
-
-@app.route('/')
+@app.route("/")
 def home():
     return "Welcome to the Flask API!"
 
-@app.route('/status')
+@app.route("/data")
+def data():
+    user = list(users)
+    return jsonify(user)
+
+@app.post("/add_user")
+def add():
+    data = request.json
+    user = {"username": data.get("username"), "name": data.get("name"), "age": data.get("age"), "city": data.get("city")}
+
+    if user["username"] is None:
+        return {"error":"Username is required"}, 400
+    users[data.get("username")] = user
+    return jsonify({'message': 'User added', 'user': user}), 201
+
+@app.route("/users/<username>")
+def user(username):
+    if username is None:
+        return jsonify({'error': 'Username is required'}), 400
+
+    if users.get(username) is None:
+        return {"error": "User not found"}, 404
+    return jsonify(users.get(username)), 200
+
+@app.route("/status")
 def status():
     return "OK"
 
-@app.route('/data')
-def get_usernames():
-    return jsonify(list(users.keys()))
-
-@app.route('/users/<username>')
-def get_user(username):
-    user = users.get(username)
-    if user:
-        return jsonify(user)
-    else:
-        return jsonify({"error": "User not found"}), 404
-
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    data = request.get_json()
-
-    username = data.get('username')
-    if not username:
-        return jsonify({"error": "Username is required"}), 400
-
-    user = {
-        "username": username,
-        "name": data.get("name"),
-        "age": data.get("age"),
-        "city": data.get("city")
-    }
-
-    users[username] = user
-
-    return jsonify({
-        "message": "User added",
-        "user": user
-    }), 201
-
 app.run()
-
