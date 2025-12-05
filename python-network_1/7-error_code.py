b10e3497b33ec8b655289fb7e6e166d0b8a7d624
amil <abeshiri.ab@gmail.com>
2025-12-05 17:22:58 +0400

    Remove unnecessary comments from 7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,6 +1,4 @@
-Fayl adı: 7-error_code.py
-100% Holberton / ALX checker-dən keçəcək – requests + sys ilə
-Python#!/usr/bin/python3
+#!/usr/bin/python3
 """
 Takes a URL, sends a request and displays the body of the response.
 If HTTP status code >= 400, prints: Error code: <code>
